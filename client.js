1e5400a086ceed5d1e81227294732e47f4ba132c
Nicholas Kotsos <nick.kotsos@gmail.com>
2020-08-27 01:42:30 +0000

    added interval functionality

--- client.js
+++ client.js
@@ -9,21 +9,56 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  conn.on('connect',(data) => {
-    console.log('data',data)
- })
 
- conn.on('connect', (data) => {
-   console.log("Successfully connected to the game server")
- })
 
- conn.on('connect',() => {
-   conn.write("Name:SAD")
- })
- conn.on('connect',() => {
-   conn.write("Move: up")
- })
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server")
+    conn.write("Name: AAA");
+    conn.write("Move: up")
+    setInterval(() => {conn.write("Move: up");  }, 1000)
+    setInterval(() => {conn.write("Move: left");  }, 3000);
+  });
+
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  
+
+
+
+
+
+
+
+
+  //   conn.on('connect',(data) => {
+//     console.log('data',data)
+//  })
+
+//  conn.on('connect', (data) => {
+//    console.log("Successfully connected to the game server")
+//  })
+
+//  conn.on('connect',() => {
+//    conn.write("Name:SAD")
+//  })
+//  conn.on('connect',() => {
+//    conn.write("Move: up")
+//  })
+//  setInterval(function() => {
+//   conn.write("Move: left")
+// }); }, 2000);
+// setTimeout(function(){ conn.on('connect',() => {
+//   conn.write("Move: down")
+// }); }, 100);
+
+
   return conn;
 }
+
+
+
 
 module.exports = {connect};
